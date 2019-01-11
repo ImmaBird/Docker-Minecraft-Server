4a1c3aad373a683d711db1b429f7df5515c9f49c
immabird <immapokeyew@gmail.com>
2019-01-11 16:23:41 -0500

    added some terraform

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+
+}
+
+resource "google_compute_instance" "server" {
+  project      = "minecraft-228318"
+  name         = "minecraft"
+  zone         = "us-east1-b"
+  machine_type = "n1-standard-1"
+
+  boot_disk {
+    initialize_params {
+      size  = 10
+      type  = "pd-standard"
+      image = "gce-uefi-images/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {}
+  }
+
+  metadata_startup_script = "docker run --rm -dP -v world:/minecraft immabird/minecraft"
+}
